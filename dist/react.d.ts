37711be67368958984ad919b532e0f2ee1e9df3d
joska <jpotin@gmail.com>
2025-01-29 21:12:29 +0100

    don't compile in js

--- dist/react.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A custom ESLint configuration for libraries that use React.
- *
- * @type {import("eslint").Linter.Config}
- * */
-declare const _default: import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray;
-export default _default;
