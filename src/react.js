d1d98147ff9ef871ba86d77cef086619812bf39f
joska <jpotin@gmail.com>
2025-01-31 17:31:02 +0100

    take advantages of tseslint.config function

--- src/react.js
+++ src/react.js
@@ -6,38 +6,35 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import baseConfig from "./base.js";
 
+const reactConfig = tseslint.config({
+  files: ["**/*.{js,jsx,ts,tsx}"],
+  extends: [
+    pluginReact.configs.flat.recommended,
+    jsxA11y.flatConfigs.recommended,
+  ],
+  languageOptions: {
+    ...pluginReact.configs.flat.recommended.languageOptions,
+    globals: {
+      ...globals.serviceworker,
+      ...globals.browser,
+    },
+  },
+  plugins: {
+    "react-hooks": pluginReactHooks,
+    "react-compiler": reactCompiler,
+  },
+  settings: { react: { version: "detect" } },
+  rules: {
+    ...pluginReactHooks.configs.recommended.rules,
+    // React scope no longer necessary with new JSX transform.
+    "react/react-in-jsx-scope": "off",
+    "react-compiler/react-compiler": "error",
+  },
+});
+
 /**
  * A custom ESLint configuration for libraries that use React.
  *
  * @type {import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray}
  * */
-export default tseslint.config(
-  baseConfig,
-  pluginReact.configs.flat.recommended,
-  jsxA11y.flatConfigs.recommended,
-  {
-    languageOptions: {
-      ...pluginReact.configs.flat.recommended.languageOptions,
-      globals: {
-        ...globals.serviceworker,
-        ...globals.browser,
-      },
-    },
-  },
-  {
-    plugins: {
-      "react-hooks": pluginReactHooks,
-      "react-compiler": reactCompiler,
-    },
-    settings: { react: { version: "detect" } },
-    rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
-      "react/react-in-jsx-scope": "off",
-      "react-compiler/react-compiler": "error",
-    },
-  },
-  {
-    ignores: ["dist/**"],
-  },
-);
+export default tseslint.config(baseConfig, reactConfig);
